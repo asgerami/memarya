7705fbed228614037a780808064764abf5793214
asgerami <amiraman467@gmail.com>
2025-02-19 15:27:08 +0300

    add Header and SideBar components for improved dashboard layout and functionality

--- app/dashboard/_components/SideBar.js
+++ app/dashboard/_components/SideBar.js
@@ -3,6 +3,7 @@ import { Progress } from "@/components/ui/progress";
 import { Layout, Shield } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import UploadpPdfDialof from "./UploadpPdfDialof";
 
 function SideBar() {
   return (
@@ -10,13 +11,14 @@ function SideBar() {
       <Image src={"/logo.png"} alt="logo" width={170} height={170} />
 
       <div className="mt-9">
-        <Button
-          style={{ backgroundColor: "#05b0fc" }}
-          className="w-full text-white"
-        >
-          + Upload PDF
-        </Button>
-
+        <UploadpPdfDialof>
+          <Button
+            style={{ backgroundColor: "#05b0fc" }}
+            className="w-full text-white"
+          >
+            + Upload PDF
+          </Button>
+        </UploadpPdfDialof>
         <div className="flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 rounded lg cursor-pointer">
           <Layout color="#05b0fc" />
           <h2>Workspace</h2>
