83985262de8420687b828a1f5f5ef9d11c13e370
asgerami <amiraman467@gmail.com>
2025-02-24 18:29:36 +0300

    update PDF loader to accept dynamic URL from request parameters

--- app/api/pdf-loader/route.js
+++ app/api/pdf-loader/route.js
@@ -2,11 +2,15 @@ import { NextResponse } from "next/server";
 import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
-const pdfUrl =
-  "https://tacit-puma-242.convex.cloud/api/storage/939d0a6c-dad3-4dfc-8cd4-147350d26a34";
+//const pdfUrl ="https://tacit-puma-242.convex.cloud/api/storage/939d0a6c-dad3-4dfc-8cd4-147350d26a34";
 
 // Load the PDF
 export async function GET(req) {
+    const reqUrl = req.url;
+    const {searchParams} = new URL(reqUrl);
+    const pdfUrl = searchParams.get("pdfUrl");
+    console.log("pdfUrl", pdfUrl);
+
   const response = await fetch(pdfUrl);
   const data = await response.blob();
   const loader = new WebPDFLoader(data);
