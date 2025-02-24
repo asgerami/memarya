0f0f974987a81d2bd4f39ce28159f82b36a23f2e
asgerami <amiraman467@gmail.com>
2025-02-24 18:29:20 +0300

    add ingest action for ConvexVectorStore with Google Generative AI embeddings

---
+++ convex/myAction.js
@@ -0,0 +1,30 @@
+import { ConvexVectorStore } from "@langchain/community/vectorstores/convex";
+import { action } from "./_generated/server.js";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { v } from "convex/values";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const ingest = action({
+  args: {
+    splitText: v.any(),
+    fileId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ConvexVectorStore.fromTexts(
+      args.splitText,
+      args.fileId,
+      new GoogleGenerativeAIEmbeddings({
+        apiKey: process.env.GOOGLE_GENAI_API_KEY,
+        model: "text-embedding-004", // 768 dimensions
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }),
+      { ctx }
+    );
+
+    return "Completed.."
+  },
+});
